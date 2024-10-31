11e790ccc0fa016b37f1a7c5cf549c6b07b46c03
AVI <vikashconnecting@gmail.com>
2024-10-31 15:46:31 +0530

    logo changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -18,10 +18,10 @@ const Navbar = () => {
 
   return (
     <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-4 md:px-10'>
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[20px]">
         <a href="#home" className="h-auto w-auto flex flex-row items-center">
           <Image 
-            src="/Navlogo.png"
+            src="/logo.png"
             alt="Logo Image"
             width={50}
             height={50}
