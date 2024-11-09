918befe48f24a1800359446d858c466dec997824
AVI <vikashconnecting@gmail.com>
2024-11-09 20:27:54 +0530

    portfolio data added

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import ProjectCards from '../sub/ProjectCards'
+import { projectDetails } from '@/constant'
 
 const Projects = () => {
   return (
@@ -8,21 +9,14 @@ const Projects = () => {
        <h1 className="pb-4 text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
         My Projects </h1> 
         <div className="w-full mt-1 h-full flex flex-col md:flex-row gap-10 px-10">
-            <ProjectCards
-            src="/NextWebsite.png"
-            title="Modern Next js portfolio"
-            description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et aliquam impedit aut tenetur mollitia pariatur fugit. Quo perferendis magni"/>
-           
-           <ProjectCards
-            src="/CardImage.png"
-            title="Interactive Website Card"
-            description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et aliquam impedit aut tenetur mollitia pariatur fugit. Quo perferendis magni"/>
-           
-           <ProjectCards
-            src="/SpaceWebsite.png"
-            title="Space themed Website"
-            description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et aliquam impedit aut tenetur mollitia pariatur fugit. Quo perferendis magni"/>
-           
+          {
+            projectDetails.map((pDetails)=>(
+              <ProjectCards
+              src={pDetails.imageSrc}
+              title={pDetails.imageTitle}
+              description={pDetails.imageDescription} />
+            ))
+          }
         </div>
     </div>
   )
